9fd5f8556a6eb2e495b970cf82070fc6458fde50
csLinhart <christoph_linhart1993@yahoo.de>
2022-12-04 06:32:41 +0100

    Refactor day 4 2022

--- 2022/D04.jl
+++ 2022/D04.jl
@@ -1,19 +1,17 @@
 input = readlines("D04.txt")
 
-function D04P1(input)
-    sum1 = sum2 = 0
+function D04(input, c)
+    sum = 0
     for e in input
         inp = split.(split(e, ','), '-')
         first = parse.(Int, inp[1])
         second = parse.(Int, inp[2])
-        if ((first[1] <= second[1] && first[2] >= second[2]) || (first[1] >= second[1] && first[2] <= second[2]))
-            sum1 += 1
-        end
-        if ((first[1] <= second[1] && first[2] >= second[1]) || (first[1] >= second[1] && first[1] <= second[2]))
-            sum2 += 1
+        if ((first[1] <= second[1] && first[2] >= second[c]) || (first[1] >= second[1] && first[c] <= second[2]))
+            sum += 1
         end
     end
-    (sum1, sum2)
+    sum
 end
 
-println(D04P1(input))
+println(D04(input, 2))
+println(D04(input, 1))
