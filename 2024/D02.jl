87f9b9dfbea532bbed281809f5758fa71356fd67
csLinhart <36882208+csLinhart@users.noreply.github.com>
2024-12-02 21:13:21 +0100

    Update D02.jl

--- 2024/D02.jl
+++ 2024/D02.jl
@@ -1,14 +1,45 @@
 input = split.(readlines("D02.txt"), " ")
 
-function D02P1(input)
-    r = 0
-    for l in input
-        d = diff(parse.(Int, l))
-        if all(x -> x < 4 && x > 0, d) || all(x -> x > -4 && x < 0, d)
-            r += 1
-        end
-    end
-    r
+function is_safe(levels)
+    diffs = diff(levels)
+    all_increasing = all(x -> x >= 1 && x <= 3, diffs)
+    all_decreasing = all(x -> x <= -1 && x >= -3, diffs)
+    return all_increasing || all_decreasing
 end
 
-D02P1(input)
+function is_safe_with_dampener(report)
+    levels = parse.(Int, report)
+    if is_safe(levels)
+        return true
+    end
+    for i in 1:length(levels)
+        new_levels = deleteat!(copy(levels), i)
+        if is_safe(new_levels)
+            return true
+        end
+    end
+    return false
+end
+
+function count_safe_reports_with_dampener(input)
+    count = 0
+    for report in input
+        if is_safe_with_dampener(report)
+            count += 1
+        end
+    end
+    return count
+end
+
+function count_safe_reports(input)
+    count = 0
+    for report in input
+        if is_safe(parse.(Int, report))
+            count += 1
+        end
+    end
+    return count
+end
+
+println(count_safe_reports(input))
+println(count_safe_reports_with_dampener(input))
